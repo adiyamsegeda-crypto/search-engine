9870fef0f2e6f90c1016bae1d2255244e1fc1116
adiyamsegeda-crypto <adiyamsegeda@gmail.com>
2025-10-03 01:16:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,55 @@
 import streamlit as st
+import ddgs
 from langchain_groq import ChatGroq
 from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
 from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
-from langchain.agents import initialize_agent,AgentType
+from langchain.agents import initialize_agent,agent_type
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
-
-## Arxiv and wikipedia Tools
-arxiv_wrapper=ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+groq_api_key=os.getenv("GROQ_API_KEY")
+arxiv_wrapper=ArxivAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
-
-api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
-wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
-
-search=DuckDuckGoSearchRun(name="Search")
-
-
-st.title("🔎 LangChain - Chat with search")
+wiki_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
+wiki=WikipediaQueryRun(api_wrapper=wiki_wrapper)
+search=DuckDuckGoSearchRun(name="search")
+st.title("LangChain -Chat with search")
 """
-In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
-"""
+in this example,we are using "StreamlitCallbackHandler" to display the thoughts of actions
+Try more langchain,streamlit agent examples on github
 
-## Sidebar for settings
+"""
 st.sidebar.title("Settings")
-api_key=st.sidebar.text_input("Enter your Groq API Key:",type="password")
-
+api_key=st.sidebar.text_input("Enter your Groq API key:",type="password")
 if "messages" not in st.session_state:
     st.session_state["messages"]=[
-        {"role":"assisstant","content":"Hi,I'm a chatbot who can search the web. How can I help you?"}
+        {"role":"assistant,"content":"Hi,i'm a chatbot who can search the web.How can i help you?}
+
     ]
-
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg['content'])
-
-if prompt:=st.chat_input(placeholder="What is machine learning?"):
+for msg in st.session.messages:
+    st.chat_message(msg["role"].write(msg["content"]))
+if prompt:=st.chat_input(placeholder="what is machine learning?"):
     st.session_state.messages.append({"role":"user","content":prompt})
     st.chat_message("user").write(prompt)
-
-    llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
+    llm=ChatGroq(api_key=groq_api_key,model="llama-3.1-8b-instant",streaming=True)
     tools=[search,arxiv,wiki]
-
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
-
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
     with st.chat_message("assistant"):
-        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=True)
         response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
-        st.session_state.messages.append({'role':'assistant',"content":response})
+        st.session_state.messages.append({"role":"assistant","content":response})
         st.write(response)
 
+        
+
+
+
+
+
+
+
+
+
+
+
+openai_api_key="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"
